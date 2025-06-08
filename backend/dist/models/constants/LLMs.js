d97c58490609c253f844141572eee28c304e488f
smallgiant1010 <smallgiant1010@gmail.com>
2025-06-08 12:03:40 -0400

    Delete backend/dist directory

--- backend/dist/models/constants/LLMs.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LLMS = void 0;
-var LLMS;
-(function (LLMS) {
-    LLMS["chat_model"] = "granite3-dense:8b";
-    LLMS["embedding_model"] = "bge-large:latest";
-    LLMS[LLMS["timeout"] = 3600] = "timeout";
-})(LLMS || (exports.LLMS = LLMS = {}));
